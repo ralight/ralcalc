50d4db54d3996525e4ebf3ebc88897bb22737b0b
Roger Light <roger@atchoo.org>
2009-01-06 12:16:35 +0000

    Revert 157:c2d53610bb86 on config.mk.

--- config.mk
+++ config.mk
@@ -2,8 +2,7 @@ VERSION=1.3.0
 BUILDDATE=20081218
 
 # Add "-DDEBUG" to CFLAGS for verbose debug output
-CFLAGS=-fno-common -Wall -Wdeclaration-after-statement -Wextra -Wformat=2 -Winit-self -Winline -Wpacked -Wp,-D_FORTIFY_SOURCE=2 -Wpointer-arith -Wlarger-than-65500 -Wmissing-declarations -Wmissing-format-attribute -Wmissing-noreturn -Wmissing-prototypes -Wnested-externs -Wold-style-definition -Wredundant-decls -Wsign-compare -Wstrict-aliasing=2 -Wstrict-prototypes -Wswitch-enum -Wundef -Wunreachable-code -Wwrite-strings -ggdb -Wall -O2
-#CFLAGS=-ggdb -Wall -O2
+CFLAGS=-ggdb -Wall -O2
 LDFLAGS=-nopie -lm
 
 CC=gcc
